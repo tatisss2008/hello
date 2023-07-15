ca34acc51a2c999a193dcf97d2b6bfb01aa8b1af
AnaM <test>
2023-07-15 12:39:45 -0500

    v.0.11.07.23

--- src/main/java/com/hello/hello/repository/StudentRepository.java
+++ src/main/java/com/hello/hello/repository/StudentRepository.java
@@ -2,9 +2,15 @@ package com.hello.hello.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+
 import com.hello.hello.entity.Student;
 
 @RepositoryRestResource(collectionResourceRel= "students", path="students")
+@CrossOrigin(origins="http://localhost:3000")
+
+
 
 // /students = GET
 // /students = POST
